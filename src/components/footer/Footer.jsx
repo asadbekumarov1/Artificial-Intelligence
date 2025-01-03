87c8160322fb284cd81d834ce23293e81dc6cbf9
asadbekumarov1 <your-email@example.com>
2025-01-03 13:17:46 +0500

    finish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,9 @@ import Linkedin from "../../assets/img/footer/linkedin.svg";
 import Youtube from "../../assets/img/footer/youtube.svg";
 import Globus from "../../assets/img/footer/globus.svg";
 import Arrow from "../../assets/img/footer/arrow.svg";
+import { useTranslation } from "react-i18next";
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <div className="bg-[#070f1d]">
       <div className="max-w-[1361px] px-4 m-auto">
@@ -21,114 +23,116 @@ const Footer = () => {
               Artificial Intelligence
             </p>
             <p className=" pb-9 font-normal max-w-[261px] text-sm leading-5 text-[#9c9fa5]">
-              Lorem ipsum is placeholder text commonly used in the graphic,
-              print, and layouts and visual mockups.
+              {t(
+                "Lorem ipsum is placeholder text commonly used in the graphic, print, and layouts and visual mockups."
+              )}
             </p>
             <div className="pb-6 flex gap-2 items-center">
               <img src={Email} alt="Email" />
               <p className="font-medium text-sm leading-4 text-white">
-                Company@gmail.com.com
+                {t("Company@gmail.com.com")}
               </p>
             </div>
             <div className="pb-6 flex gap-2 items-center">
               <img src={Phone} alt="Phone" />
               <p className="font-medium text-sm leading-4 text-white">
-                Phone: (064) 332-1233
+                {t("Phone: (064) 332-1233")}
               </p>
             </div>
             <div className="pb-6 flex gap-2 items-center">
               <img src={Location} alt="Location" />
               <p className="font-medium text-sm leading-4 text-white">
-                450 Wall Street, USA, New York
+                {t("450 Wall Street, USA, New York")}
               </p>
             </div>
           </div>
           <div>
             <p className="pb-8 font-medium text-base leading-5 text-white">
-              INFORMATION
+              {t("INFORMATION")}
             </p>
             <ul className="flex flex-col gap-5">
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                New Collection
+                {t("New Collection")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                About Store
+                {t("About Store")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Contact Us
+                {t("Contact Us")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Latest News
+                {t("Latest News")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Our Sitemap
+                {t("Our Sitemap")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Orders History
+                {t("Orders History")}
               </li>
             </ul>
           </div>
           <div>
             <p className=" pb-8 font-medium text-base leading-5 text-white">
-              FOOTER MENU
+              {t("FOOTER MENU")}
             </p>
             <ul className="flex flex-col gap-5">
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Instagram profile
+                {t("Instagram profile")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                New Collection
+                {t("New Collection")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Contact Us
+                {t("Contact Us")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Latest News
+                {t("Latest News")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Terms & Conditions
+                {t("Terms & Conditions")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Purchase Theme
+                {t("Purchase Theme")}
               </li>
             </ul>
           </div>
           <div>
             <p className="font-medium pb-8 text-base leading-5 text-white">
-              USEFUL LINKS
+              {t("USEFUL LINKS")}
             </p>
             <ul className="flex flex-col gap-5">
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Instagram profile
+                {t("Instagram profile")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                New Collection
+                {t("New Collection")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Contact Us
+                {t("Contact Us")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Latest News
+                {t("Latest News")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Terms & Conditions
+                {t("Terms & Conditions")}
               </li>
               <li className="text-[#9c9fa5] font-medium text-sm ">
-                Purchase Theme
+                {t("Purchase Theme")}
               </li>
             </ul>
           </div>
           <div>
             <p className="font-medium pb-8 text-base leading-5 text-white">
-              ABOUT THE STORE
+              {t("ABOUT THE STORE")}
             </p>
             <div className="flex flex-col gap-5">
               <p className="text-[#9c9fa5] font-medium text-sm max-w-[261px]">
-                Lorem ipsum is placeholder text commonly used in the graphic,
-                print, and layouts and visual mockups.
+                {t(
+                  "Lorem ipsum is placeholder text commonly used in the graphic, print, and layouts and visual mockups."
+                )}
               </p>
               <p className="font-medium text-sm leading-4 text-white">
-                www.company.com
+                {t("www.company.com")}
               </p>
               <div className="flex gap-6 pb-[52px] items-center">
                 <img src={Facebook} alt="Facebook" />
@@ -140,7 +144,7 @@ const Footer = () => {
               <div className="flex gap-2 items-center">
                 <img src={Globus} alt="Globus" />
                 <p className="font-medium text-sm leading-4 text-white">
-                  English
+                  {t("English")}
                 </p>
                 <img src={Arrow} alt="Arrow" />
               </div>
